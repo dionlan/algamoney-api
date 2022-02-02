8ca8263593522abf803706502db7fb5f870ebb52
dionlan <dionlan.alves@gmail.com>
2022-02-02 16:34:06 -0300

    ResponseEntity noConten when list isEmpty.

--- src/main/java/com/dionlan/algamoney/api/resource/CategoryResource.java
+++ src/main/java/com/dionlan/algamoney/api/resource/CategoryResource.java
@@ -3,6 +3,7 @@ package com.dionlan.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,8 @@ public class CategoryResource {
 	private CategoryRepository repository;
 	
 	@GetMapping
-	public List<Category> listar(){
-		return repository.findAll();
-		
+	public ResponseEntity<?> list(){
+		List<Category> categories = repository.findAll();
+		return !categories.isEmpty() ? ResponseEntity.ok(categories) : ResponseEntity.noContent().build();
 	}
-}
+} 
