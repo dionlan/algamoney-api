645afc9e4b76326d3b3bad42b5ee236b1a40b857
dionlan <dionlan.alves@gmail.com>
2022-02-05 19:08:31 -0300

    Configure OAuth AuthorizationServerConfig, base security and CustomTokenEnhancer.

--- src/main/java/com/dionlan/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/dionlan/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,54 +1,80 @@
 package com.dionlan.algamoney.api.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@Configuration
-@EnableAuthorizationServer
-public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-	
-    @Autowired
-	private AuthenticationManager authenticationManager;
+import com.dionlan.algamoney.api.config.token.CustomTokenEnhancer;
 
-	/**
-	 * App Client Authorization
-	 */
+@Profile("oauth-security")
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+		
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 				.withClient("angular")
-				.secret("@ngul@r0")
+				.secret("@ngul@r0") // @ngul@r0				
 				.scopes("read", "write")
 				.authorizedGrantTypes("password", "refresh_token")
-				.refreshTokenValiditySeconds(20)
+				.accessTokenValiditySeconds(3600)
+				.refreshTokenValiditySeconds(3600 * 24)
+			.and()
+				.withClient("mobile")
+				.secret("m0b1l30") // m0b1l30
+				.scopes("read")
+				.authorizedGrantTypes("password", "refresh_token")
+				.accessTokenValiditySeconds(3600)
 				.refreshTokenValiditySeconds(3600 * 24);
 	}
-
+	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), accessTokenConverter()));
+		
 		endpoints
 			.tokenStore(tokenStore())
-			.accessTokenConverter(accessTokenConverter())
+			.tokenEnhancer(tokenEnhancerChain)
+			.reuseRefreshTokens(false)
+			.userDetailsService(userDetailsService)
 			.authenticationManager(authenticationManager);
 	}
 	
-	private JwtAccessTokenConverter accessTokenConverter() {
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
-		accessTokenConverter.setSigningKey("dionlan");
+		accessTokenConverter.setSigningKey("algaworks");
 		return accessTokenConverter;
 	}
 
 	@Bean
 	public TokenStore tokenStore() {
-		return new  JwtTokenStore(accessTokenConverter());
+		return new JwtTokenStore(accessTokenConverter());
 	}
+	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return new CustomTokenEnhancer();
+	}
+	
 }
